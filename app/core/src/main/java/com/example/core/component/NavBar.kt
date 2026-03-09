7c17f5f2d970c9142e3d85cedf3cc2602d208b5a
yasirkhoirul <yasirkhoirul9@gmail.com>
2026-03-09 20:55:29 +0700

    adding theme into core

--- app/core/src/main/java/com/example/core/component/NavBar.kt
+++ app/core/src/main/java/com/example/core/component/NavBar.kt
@@ -1,4 +1,22 @@
 package com.example.core.component
 
-class NavBar {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NavBar(modifier: Modifier = Modifier) {
+    Box(){
+        Row() {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NavBarPrev() {
+
 }
